d576bfbec20fcba0c018dbdfbb4762c43ca697b3
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-06-20 10:26:26 +0500

    Data Annotation

--- Web/Models/Category.cs
+++ Web/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
