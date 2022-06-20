d9d3374fade9f54a92107f1520eb1c8f5071917d
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-06-20 10:37:41 +0500

    Data Context File

---
+++ Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
