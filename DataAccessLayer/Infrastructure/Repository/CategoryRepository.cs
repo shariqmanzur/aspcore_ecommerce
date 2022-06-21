a492b2b1d42f0744b3c71091e89ceb3d064f9c21
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-06-21 20:40:26 +0500

    Repository Pattern

---
+++ DataAccessLayer/Infrastructure/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Infrastructure.IRepository;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Infrastructure.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+    }
+}
---
+++ DataAccessLayer/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Infrastructure.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetT(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression).FirstOrDefault();
+        }
+    }
+}
