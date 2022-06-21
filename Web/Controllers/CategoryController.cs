c4bc8a805a6fc23b835bd902cc75f9b28d6a9edf
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-06-21 09:52:18 +0500

    Edit Get Method

--- Web/Controllers/CategoryController.cs
+++ Web/Controllers/CategoryController.cs
@@ -37,5 +37,20 @@ namespace Web.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
     }
 }
