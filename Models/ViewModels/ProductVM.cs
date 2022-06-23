91052d297e4c901cac193dd9faafc23442a33513
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-06-23 10:23:03 +0500

    Create Product Get Method

---
+++ Models/ViewModels/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
---
+++ Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Infrastructure.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models;
+using Models.ViewModels;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            ProductVM productVM = new ProductVM();
+            productVM.Products = _unitOfWork.Product.GetAll();
+            return View(productVM);
+        }
+
+        [HttpGet]
+        public IActionResult CreateEdit(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new(),
+                Categories = _unitOfWork.Category.GetAll().Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _unitOfWork.Product.GetT(x => x.Id == id);
+                if(productVM == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(productVM);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateEdit(CategoryVM categoryVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if(categoryVM.Category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(categoryVM.Category);
+                    TempData["success"] = "Category Created Done!";
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(categoryVM.Category);
+                    TempData["success"] = "Category Updated Done!";
+                }                
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitOfWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _unitOfWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Delete(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted Done!";
+            return RedirectToAction("Index");
+        }
+    }
+}
