8cc7d93cf7b51f4ce21c4ec8afe6512994b592f3
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-07-13 20:30:15 +0500

    Identity Customization

--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace DataAccessLayer.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PinCode { get; set; }
+
+    }
+}
