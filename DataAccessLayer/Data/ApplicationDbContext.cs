740659933bde9f036ab60545addbd87a17b2afd7
mdshariqmanzoor <mdshariqmanzoor@gmail.com>
2022-08-17 12:51:48 +0500

    Add Both tables to Database

--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -15,5 +15,8 @@ namespace DataAccessLayer.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Cart> Carts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
     }
 }
